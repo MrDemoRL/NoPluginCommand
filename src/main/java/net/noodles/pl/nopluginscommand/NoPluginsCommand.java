3f47544f131c1eddf758656ba0152f986d248cf3
MrDemoRL <83421334+MrDemoRL@users.noreply.github.com>
2021-04-29 19:12:00 +0200

    yes.java
    yes

--- src/main/java/net/noodles/pl/nopluginscommand/NoPluginsCommand.java
+++ src/main/java/net/noodles/pl/nopluginscommand/NoPluginsCommand.java
@@ -26,9 +26,9 @@ public final class NoPluginsCommand extends JavaPlugin implements Listener {
     @EventHandler
     public void onCommandUse(PlayerCommandPreprocessEvent event) {
         Player player = event.getPlayer();
-        List<String> commands = Arrays.asList("pl", "about", "version", "ver", "plugins", "bukkit:pl", "bukkit:about", "bukkit:version", "bukkit:ver", "bukkit:plugins", "minecraft:pl", "minecraft:plugins", "minecraft:about", "minecraft:version", "minecraft:ver");
+        List<String> commands = Arrays.asList("pl", "about", "version", "ver", "plugins", "bukkit:pl", "bukkit:about", "bukkit:version", "bukkit:ver", "bukkit:plugins", "minecraft:pl", "minecraft:plugins", "minecraft:about", "minecraft:version", "minecraft:ver", "calc");
         commands.forEach(all -> {
-         if (event.getMessage().toLowerCase().equalsIgnoreCase("/" + all.toLowerCase())) {
+         if (event.getMessage().toLowerCase().equalsIgnoreCase("/" or "//" + all.toLowerCase())) {
              event.setCancelled(true);
          }
         });
